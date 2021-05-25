1443a824c191ed68a6fc0fecf3108fb43e6dc135
John Santos <84046012+johnsanttos@users.noreply.github.com>
2021-05-25 19:35:52 -0300

    Commit

---
+++ calculate-react-main/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Calculate } from "./components/containers/calculate";
+import "./App.css";
+
+export function App() {
+  return (
+    <div className={"screen"}>
+      <div />
+      <Calculate />
+      <div />
+    </div>
+  );
+}
